3e8618cb71a81174710523c01178401ccbe0e1ba
Muhammadsodiq Shukirillayev <newmclight021226@gmail.com>
2025-06-30 07:58:36 +0900

    Fix Render deployment configuration - update WSGI/ASGI paths and improve render.yaml

--- remProdKitchen/remProdKitchen/wsgi.py
+++ remProdKitchen/remProdKitchen/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "remProdKitchen.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "remProdKitchen.remProdKitchen.settings")
 
 application = get_wsgi_application()
