fd12d7e192bdb8f5669e31a5711177713a4134ec
Muhammadsodiq Shukirillayev <newmclight021226@gmail.com>
2025-06-30 08:21:50 +0900

    Change deployment approach - run from remProdKitchen directory with simpler paths

--- remProdKitchen/remProdKitchen/asgi.py
+++ remProdKitchen/remProdKitchen/asgi.py
@@ -11,6 +11,7 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "remProdKitchen.remProdKitchen.settings")
+# Explicitly set the settings module
+os.environ["DJANGO_SETTINGS_MODULE"] = "remProdKitchen.settings"
 
 application = get_asgi_application()
--- remProdKitchen/remProdKitchen/wsgi.py
+++ remProdKitchen/remProdKitchen/wsgi.py
@@ -11,6 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "remProdKitchen.remProdKitchen.settings")
+# Explicitly set the settings module
+os.environ["DJANGO_SETTINGS_MODULE"] = "remProdKitchen.settings"
 
 application = get_wsgi_application()
